72eac9876f9d7077d29e7f8711baa5b9fe6e3c8a
Jose Martin Bellido <a_jose.martin.bellido@iespablopicasso.es>
2023-10-25 13:46:50 +0200

    Añadimos método suma en el archivo java

--- ProyectoSumaED/src/tarea1EDGit/SumaDeEnteros.java
+++ ProyectoSumaED/src/tarea1EDGit/SumaDeEnteros.java
@@ -1,6 +1,25 @@
 package tarea1EDGit;
 
+import java.util.Scanner;
+
 public class SumaDeEnteros {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// Recogemos los datos
+		System.out.println("SUMA DE DOS ENTEROS");
+		System.out.print("Introduce un primer número entero");
+		int numero1 = sc.nextInt();
+		
+		System.out.print("Introduce un segundo número entero");
+		int numero2 = sc.nextInt();
+		
+		// Impresión de resultado con el calculo interno en el print
+		System.out.println("La suma de " + numero1 + " con " + numero2 + " da como resultado: " + (numero1 + numero2));
+		
+		sc.close();
+	}
 
 }
